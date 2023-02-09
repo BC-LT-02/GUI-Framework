42f55de5d443778e2130b854f615a818f81f71af
Jorge Chavez <ljorchavez@gmail.com>
2023-02-09 14:44:40 -0600

    Added Remote Web Driver, docker-compose for selenium grid and added step to workflow

---
+++ 1.Core/UIElements/Drivers/RemoteWebDriver.cs
@@ -0,0 +1,42 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Safari;
+
+namespace SeleniumTest.Core.Drivers;
+
+public class WebDriverFactory
+{
+    public static IWebDriver GetDriver(string driverType)
+    {
+        switch (driverType)
+        {
+            case "Chrome":
+                var chrome = new ChromeOptions();
+                return RemoteConfigs(chrome);
+            case "Edge":
+                var edge = new EdgeOptions();
+                return RemoteConfigs(edge);
+            case "Safari":
+                var safari = new SafariOptions();
+                return RemoteConfigs(safari);
+            case "Firefox":
+                var firefox = new FirefoxOptions();
+                return RemoteConfigs(firefox);
+            default:
+                throw new NotImplementedException("Error specifying the driver");
+        }
+    }
+
+    public static IWebDriver RemoteConfigs(DriverOptions capabilities)
+    {
+        var driver = new RemoteWebDriver(new Uri("http://localhost:4444/"), capabilities);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(
+            ConfigModel.DriverExplicitTimeout
+        );
+        driver.Manage().Window.Maximize();
+        return driver;
+    }
+}
