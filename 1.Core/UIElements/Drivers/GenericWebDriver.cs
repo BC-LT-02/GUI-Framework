acd97ec2f3f2287d534d8cb4af231048ac6f5d19
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2023-02-09 15:01:12 -0300

    Refactor GenericWebDriver and remove visible condition in UIElements

--- 1.Core/UIElements/Drivers/GenericWebDriver.cs
+++ 1.Core/UIElements/Drivers/GenericWebDriver.cs
@@ -12,7 +12,7 @@ public class GenericWebDriver
         _driver =
             _driver == null ? RemoteWebDriverFactory.GetDriver(ConfigModel.DriverType) : _driver;
 
-    public static WebDriverWait Wait => new WebDriverWait(Instance, TimeSpan.FromSeconds(30));
+    public static WebDriverWait Wait => new WebDriverWait(Instance, TimeSpan.FromSeconds(ConfigModel.DriverExplicitTimeout));
 
     public static void Dispose()
     {
