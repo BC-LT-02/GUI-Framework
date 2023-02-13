30a7c28cc61a731a42e6861508c5851e3f76be1a
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2023-02-13 19:34:27 -0300

    Refactor user delete/put step definitions

--- 3.Tests/API/StepDefinitions/User/PutStepDefinitions.cs
+++ 3.Tests/API/StepDefinitions/User/PutStepDefinitions.cs
@@ -1,99 +1,46 @@
-﻿// using System.Text.Json;
-// using RestSharp;
-// using TechTalk.SpecFlow;
-// using Todoly.Tests.API.Steps.Commons;
-// using Todoly.Views.Models;
+﻿using System.Text.Json;
+using RestSharp;
+using TechTalk.SpecFlow;
+using Todoly.Core.Helpers;
+using Todoly.Tests.API.Steps.Commons;
+using Todoly.Views.Models;
 
-// namespace Todoly.Tests.API.Steps.User
-// {
-//     [Binding]
-//     [Scope(Feature = "User Update")]
-//     public class PutStepDefinitions : CommonSteps
-//     {
-//         private readonly ScenarioContext _scenarioContext;
+namespace Todoly.Tests.API.Steps.User
+{
+    [Binding]
+    [Scope(Feature = "User Update")]
+    public class PutStepDefinitions : CommonSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
 
-//         public PutStepDefinitions(ScenarioContext scenarioContext) : base(scenarioContext)
-//         {
-//             _scenarioContext = scenarioContext;
-//         }
+        public PutStepDefinitions(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
 
-//         [When(@"the user submits a PUT request to ""(.*)"" with a valid JSON body")]
-//         public void WhentheusersubmitsaPUTrequesttowithavalidJSONbody(string url, string body)
-//         {
-//             Client.AddDefaultHeader("Authorization", _scenarioContext["Authorization"].ToString()!);
-//             Client.AddDefaultHeader("Accept", "*/*");
+        [When(@"the user submits a PUT request to ""(.*)"" with a valid JSON body")]
+        public void WhentheusersubmitsaPUTrequesttowithavalidJSONbody(string endpoint, string body)
+        {
+            _scenarioContext["Response"] = Client.DoRequest(Method.Put, endpoint, body);
+        }
 
-//             UserModel? user = Newtonsoft.Json.JsonConvert.DeserializeObject<UserModel>(body);
-//             _scenarioContext["Response"] = Client.Put<UserModel>(url, user);
-//         }
+        [Then(@"the API should return a ""(.*)"" response with the updated user information")]
+        public void ThentheAPIshouldreturnaresponsewiththeupdateduserinformation(string statusCode)
+        {
+            RestResponse response = (RestResponse)_scenarioContext["Response"];
+            Assert.True(response.IsSuccessful);
+            Assert.Equal(statusCode, response.StatusCode.ToString());
+            var user = JsonSerializer.Deserialize<UserModel>(response.Content!);
+            Assert.IsType<UserModel>(user);
+            Assert.Equal("New Name", user.FullName);
+        }
 
-//         [Then(@"the API should return a ""(.*)"" status code and the updated user information in JSON format")]
-//         public void ThentheAPIshouldreturnastatuscodeandtheupdateduserinformationinJSONformat(string statusCode)
-//         {
-//             RestResponse response = (RestResponse)_scenarioContext["Response"];
-//             Assert.True(response.IsSuccessful);
-//             Assert.Equal(statusCode, response.StatusCode.ToString());
-//             var user = JsonSerializer.Deserialize<UserModel>(response.Content!);
-//             Assert.IsType<UserModel>(user);
-//             Assert.Equal("New Name", user.FullName);
-//         }
-
-//         [When(@"the user submits a PUT request to ""(.*)"" with an invalid ""(.*)"" user email")]
-//         public void WhentheusersubmitsaPUTrequesttowithaninvaliduseremail(string url, string email)
-//         {
-//             Client.AddDefaultHeader("Authorization", "Basic aW52YWxpZEBlbWFpbC5jb206UGFzc3dvcmQ=");
-//             Client.AddDefaultHeader("Accept", "*/*");
-
-//             UserModel body = new UserModel(
-//                 email,
-//                 "newPassword",
-//                 null,
-//                 null,
-//                 null,
-//                 null,
-//                 null,
-//                 null,
-//                 null,
-//                 null,
-//                 null,
-//                 null,
-//                 null
-//             );
-//             _scenarioContext["Response"] = Client.Put<UserModel>(url, body);
-//         }
-
-//         [Then(@"the API should return a ""(.*)"" response with a (.*) status code and a ""(.*)"" error message indicating that the user was not found")]
-//         public void ThentheAPIshouldreturnaresponsewithastatuscodeandaerrormessageindicatingthattheuserwasnotfound(string response, int statusCode, string message)
-//         {
-//             RestResponse res = (RestResponse)_scenarioContext["Response"];
-
-//             Assert.True(res.IsSuccessful);
-//             Assert.Equal(response, res.StatusCode.ToString());
-
-//             var error = JsonSerializer.Deserialize<ErrorModel>(res.Content!);
-//             Assert.IsType<ErrorModel>(error);
-//             Assert.Equal(message, error!.ErrorMessage);
-//             Assert.Equal(statusCode, error!.ErrorCode);
-//         }
-
-//         [When(@"the user submits a PUT request to ""(.*)""")]
-//         public void WhentheusersubmitsaPUTrequestto(string url)
-//         {
-//             _scenarioContext["Response"] = Client.Put<UserModel>(url, body: null!);
-//         }
-
-//         [Then(@"the API should return a ""(.*)"" response with a (.*) status code and a ""(.*)"" error message indicating that the user is not authorized to access the resource.")]
-//         public void ThentheAPIshouldreturnaresponsewithastatuscodeandaerrormessageindicatingthattheuserisnotauthorizedtoaccesstheresource(string response, int statusCode, string message)
-//         {
-//             RestResponse res = (RestResponse)_scenarioContext["Response"];
-
-//             Assert.True(res.IsSuccessful);
-//             Assert.Equal(response, res.StatusCode.ToString());
-
-//             var error = JsonSerializer.Deserialize<ErrorModel>(res.Content!);
-//             Assert.IsType<ErrorModel>(error);
-//             Assert.Equal(message, error!.ErrorMessage);
-//             Assert.Equal(statusCode, error!.ErrorCode);
-//         }
-//     }
-// }
+        [AfterScenario("update.user.fullname")]
+        public void UpdateUserFullName()
+        {
+            Client.AddAuthenticator(ConfigBuilder.Instance.GetString("TODO-LY-EMAIL"), ConfigBuilder.Instance.GetString("TODO-LY-PASS"));
+            string body = "{ \"FullName\": \"Base Name\" }";
+            Client.DoRequest(Method.Put, "/user/0.json", body);
+        }
+    }
+}
