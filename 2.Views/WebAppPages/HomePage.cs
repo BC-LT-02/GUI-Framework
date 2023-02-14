7a3d67e6ff628b888012060781574b4e2830020f
Adriel Amorim <101124920+adrielgamorim@users.noreply.github.com>
2023-02-13 23:34:24 -0300

    Added `Post` and `Update` project tests (#30)
    * Added 'Post' and 'Update' project tests
    
    * Implemented changes requested

--- 2.Views/WebAppPages/HomePage.cs
+++ 2.Views/WebAppPages/HomePage.cs
@@ -14,16 +14,69 @@ public class HomePage
 
     public IClickable SettingsButton => new Button("", new Locator(LocatorType.XPath, "//div[@id='ctl00_HeaderTopControl1_PanelHeaderButtons']//a[text()='Settings']"));
 
+    public IClickable AddNewProjectButton =>
+        new Button("", new Locator(LocatorType.XPath, "//td[@class='ProjItemContent' and text()='Add New Project']"));
+
+    public ITypeable AddNewProjectInput =>
+        new TextField("", new Locator(LocatorType.Id, "NewProjNameInput"));
+
+    public IClickable AddNewProjectNameButton =>
+        new Button("", new Locator(LocatorType.Id, "NewProjNameButton"));
+
+    public IElement CurrentSelectedProject =>
+        new BaseWebElement("", new Locator(LocatorType.ClassName, "ProjectSelected"));
+
+    public IClickable ProjectEditButton =>
+        new Button("", new Locator(LocatorType.XPath, "//ul[@id='projectContextMenu']/li[@class='edit']/a"));
+
+    public ITypeable ProjectEditInput =>
+        new TextField("", new Locator(LocatorType.XPath, "(//div[@id='ProjectEditDiv'])[1]/input"));
+
+    public IClickable ProjectEditSaveButton =>
+        new Button("", new Locator(LocatorType.XPath, "(//div[@id='ProjectEditDiv'])[1]/img[@id='ItemEditSubmit']"));
+
+    public IClickable ProjectEditCancelButton =>
+        new Button("", new Locator(LocatorType.XPath, "(//div[@id='ProjectEditDiv'])[1]/img[@id='ItemEditCancel']"));
+
+    public IClickable ProjectDeleteButton =>
+        new Button("", new Locator(LocatorType.Id, "ProjShareMenuDel"));
+
     public IElement ProjectTitleDiv =>
         new Button("", new Locator(LocatorType.Id, "CurrentProjectTitle"));
+
+    public IClickable NewItemAddButton =>
+        new Button("", new Locator(LocatorType.Id, "NewItemAddButton"));
+
     public IElement RecycleBinDiv => new Button("", new Locator(LocatorType.Id, "ItemId_-3"));
 
     public IClickable RecycleBinContextButton =>
         new Button("", new Locator(LocatorType.XPath, "//div[@itemid='-3']/img"));
+
     public IClickable RecycleBinEmptyButton =>
         new Button("", new Locator(LocatorType.XPath, "//ul[@id='recycleContextMenu']/li/a"));
 
     public IElement NoItemsDiv => new Button("", new Locator(LocatorType.ClassName, "NoItems"));
+
+    public IElement GetProjectTd(string projectName) =>
+        new Button("", new Locator(
+            LocatorType.XPath,
+            $"//div[@id='ProjectListPlaceHolder']//td[text()='{projectName}']"
+        )
+    );
+
+    public IElement GetProjectHandle(string projectName) =>
+        new Button("", new Locator(
+            LocatorType.XPath,
+            $"//div[@id='ProjectListPlaceHolder']//td[text()='{projectName}']/parent::tr/td/img[contains(@class, 'handle')]"
+        )
+    );
+
+    public IClickable GetProjectContextButton(string projectName) =>
+        new Button("", new Locator(
+            LocatorType.XPath,
+            $"//td[text()='{projectName}']/ancestor::table[@class='ProjItemTable']//img[@title='Options']"
+        )
+    );
 
     public HomePage() { }
 }
---
+++ 3.Tests/UI/StepDefinitions/Project/DragAndDropStepDefinitions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SeleniumExtras.WaitHelpers;
+using TechTalk.SpecFlow;
+using Todoly.Core.Helpers;
+using Todoly.Core.UIElements.Drivers;
+using Todoly.Core.UIElements.WebActions;
+using Todoly.Tests.UI.Steps.Commons;
+using Todoly.Views.WebAppPages;
+
+namespace SeleniumTest.Tests.Steps.Project;
+
+[Binding]
+[Scope(Feature = "Project drag and drop actions")]
+public class DragAndDropStepDefinitions : CommonSteps
+{
+    private readonly HomePage _homePage;
+    private readonly ScenarioContext _scenarioContext;
+
+    public DragAndDropStepDefinitions(ScenarioContext scenarioContext) : base(scenarioContext)
+    {
+        _homePage = new HomePage();
+        _scenarioContext = scenarioContext;
+    }
+
+    [When(@"the user drags and drop a project on top of another project of the list")]
+    public void DragAndDropOnTop()
+    {
+        _scenarioContext.TryGetValue(ConfigModel.CurrentProject, out string projectName);
+        var source = _homePage.GetProjectHandle(projectName);
+        var target = _homePage.GetProjectHandle("Work");
+        WebActions.DragAndDrop(source.WebElement, target.WebElement);
+    }
+
+    [Then(@"the project should be displayed as a child of the other project")]
+    public void VerifyProjectUpdate()
+    {
+        _scenarioContext.Pending();
+    }
+}
