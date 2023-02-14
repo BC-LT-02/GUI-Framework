3a3ba19f24230a002e764682fe1864af43daf23e
Sch <Schlvf>
2023-02-14 11:24:16 -0500

    Added clean up hook and delete projects script

---
+++ 1.Core/Helpers/Scripts/APIScripts.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using RestSharp;
+using Todoly.Views.Models;
+
+namespace Todoly.Core.Helpers;
+
+public class APIScrits
+{
+    public static void RemoveAllProjects()
+    {
+        RestHelper client = new RestHelper(ConfigModel.ApiHostUrl);
+        client.AddAuthenticator(ConfigModel.TODO_LY_EMAIL, ConfigModel.TODO_LY_PASS);
+        RestResponse response = client.DoRequest(Method.Get, ConfigModel.ProjectUri, null);
+        var projects = JsonSerializer.Deserialize<IEnumerable<ProjectModel>>(response.Content!);
+
+        foreach (var x in projects!)
+        {
+            string url = $"projects/{x.Id}.json";
+            RestResponse res = client.DoRequest(Method.Delete, url, null);
+            if (!res.IsSuccessful)
+            {
+                throw new Exception("A problem occurred deleting the projects");
+            }
+        }
+    }
+}
