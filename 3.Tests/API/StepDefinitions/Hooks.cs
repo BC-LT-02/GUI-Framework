723d2c9a9f3545ef7282bdfce4e24b305cbe8ba4
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2023-02-13 21:46:07 -0300

    Refactor Project Step Definitions

---
+++ 3.Tests/API/StepDefinitions/Hooks.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using RestSharp;
+using TechTalk.SpecFlow;
+using Todoly.Core.Helpers;
+using Todoly.Views.Models;
+
+namespace Todoly.Tests.API.Steps.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        private readonly ScenarioContext _scenarioContext;
+        public readonly RestHelper Client;
+
+        public Hooks(ScenarioContext scenarioContext)
+        {
+            Client = new RestHelper(ConfigBuilder.Instance.GetString("api", "ApiHostUrl"));
+            _scenarioContext = scenarioContext;
+        }
+
+        [BeforeScenario("create.project")]
+        public void CreateProject()
+        {
+            Client.AddAuthenticator(ConfigBuilder.Instance.GetString("TODO-LY-EMAIL"), ConfigBuilder.Instance.GetString("TODO-LY-PASS"));
+            string body = "{ \"Content\": \"New Project\" }";
+            var response = Client.DoRequest(Method.Post, "/projects.json", body);
+            _scenarioContext.Add(ConfigBuilder.Instance.GetString("api", "CurrentProject"), response);
+        }
+
+        [AfterScenario("delete.project")]
+        public void DeleteProject()
+        {
+            RestResponse response = (RestResponse)_scenarioContext["Response"];
+            var projectId = JsonSerializer.Deserialize<ProjectModel>(response.Content!)!.Id.ToString();
+            Client.AddAuthenticator(ConfigBuilder.Instance.GetString("TODO-LY-EMAIL"), ConfigBuilder.Instance.GetString("TODO-LY-PASS"));
+            Client.DoRequest(Method.Delete, $"projects/{projectId}.json", null);
+        }
+    }
+}
--- 3.Tests/API/StepDefinitions/Project/GetAllStepDefinitions.cs
+++ 3.Tests/API/StepDefinitions/Project/GetAllStepDefinitions.cs
@@ -1,62 +1,43 @@
-﻿// using System.Text.Json;
-// using RestSharp;
-// using TechTalk.SpecFlow;
-// using Todoly.Tests.API.Steps.Commons;
-// using Todoly.Views.Models;
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using RestSharp;
+using TechTalk.SpecFlow;
+using Todoly.Tests.API.Steps.Commons;
+using Todoly.Views.Models;
 
-// namespace Todoly.Tests.API.Steps.Project
-// {
-//     [Binding]
-//     [Scope(Feature = "Retrieve all existing project")]
-//     public class GetAllStepDefinitions : CommonSteps
-//     {
-//         private readonly ScenarioContext _scenarioContext;
-//         private readonly string url = "projects.json";
+namespace Todoly.Tests.API.Steps.Project
+{
+    [Binding]
+    [Scope(Feature = "Retrieve all existing projects")]
+    public class GetAllStepDefinitions : CommonSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
 
-//         public GetAllStepDefinitions(ScenarioContext scenarioContext) : base(scenarioContext)
-//         {
-//             _scenarioContext = scenarioContext;
-//         }
+        public GetAllStepDefinitions(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
 
-//         [When(@"the user sends a GET request to the endpoint")]
-//         public void WhentheusersendsaGETrequesttotheendpoint()
-//         {
-//             Client.AddDefaultHeader("Authorization", _scenarioContext["Authorization"].ToString()!);
-//             Client.AddDefaultHeader("Accept", "*/*");
+        [When(@"the user submits a GET request to ""(.*)""")]
+        public void WhentheusersubmitsaGETrequestto(string endpoint)
+        {
+            _scenarioContext["Response"] = Client.DoRequest(Method.Get, endpoint, null);
+        }
 
-//             _scenarioContext["Response"] = Client.Get(url);
-//         }
+        [Then(@"the API should return a ""(.*)"" response with the list of all the projects")]
+        public void ThentheAPIshouldreturnaresponsewiththelistofalltheprojects(string response)
+        {
+            RestResponse res = (RestResponse)_scenarioContext["Response"];
+            Assert.True(res.IsSuccessful);
+            Assert.Equal(response, res.StatusCode.ToString());
+            var projects = JsonSerializer.Deserialize<List<ProjectModel>>(res.Content!);
 
-//         [Then(@"the response should have a status code of ""(.*)"" and the response should contain a list of all projects")]
-//         public void Thentheresponseshouldhaveastatuscodeofandtheresponseshouldcontainalistofallprojects(string statusCode)
-//         {
-//             RestResponse response = (RestResponse)_scenarioContext["Response"];
-//             Assert.Equal(statusCode, response.StatusCode.ToString());
-
-//             var user = JsonSerializer.Deserialize<ProjectModel>(response.Content!);
-//             Assert.IsType<ProjectModel>(user);
-//         }
-
-//         [When(@"the user sends a GET request to the api endpoint")]
-//         public void WhentheusersendsaGETrequesttotheapiendpoint()
-//         {
-//             Client.AddDefaultHeader("Accept", "*/*");
-
-//             _scenarioContext["Response"] = Client.Get(url);
-//         }
-
-//         [Then(@"the response should have a status code of ""(.*)"" and a ""(.*)"" error message indicating that the user is not authorized to access the resource.")]
-//         public void Thentheresponseshouldhaveastatuscodeofandaerrormessageindicatingthattheuserisnotauthorizedtoaccesstheresource(string statusCode, string errorMessage)
-//         {
-//             RestResponse response = (RestResponse)_scenarioContext["Response"];
-
-//             Assert.True(response.IsSuccessful);
-//             Assert.Equal(statusCode, response.StatusCode.ToString());
-
-//             var error = JsonSerializer.Deserialize<ErrorModel>(response.Content!);
-//             Assert.IsType<ErrorModel>(error);
-//             Assert.Equal(errorMessage, error!.ErrorMessage);
-//             Assert.Equal(102, error!.ErrorCode);
-//         }
-//     }
-// }
+            Assert.IsType<List<ProjectModel>>(projects);
+            foreach (ProjectModel project in projects)
+            {
+                Assert.NotNull(project.Content);
+                Assert.NotNull(project.Id);
+            }
+        }
+    }
+}
