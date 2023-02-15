3da44d67eedb518becd1a2721c8fb52b4595613b
Adriel Amorim <agimenescomercial@gmail.com>
2023-02-15 09:08:45 -0300

    Changed type of page objects

--- 2.Views/WebAppPages/Settings/ProfilePage.cs
+++ 2.Views/WebAppPages/Settings/ProfilePage.cs
@@ -7,9 +7,9 @@ namespace Todoly.Views.WebAppPages;
 
 public class ProfilePage
 {
-    public ITypeable FullNameTextField => new TextField("", new Locator(LocatorType.Id, "FullNameInput"));
-    public ITypeable EmailTextField => new TextField("", new Locator(LocatorType.Id, "EmailInput"));
-    public IClickable OkButton => new Button("", new Locator(LocatorType.XPath, "//div[@class='ui-dialog-buttonset']//span[text()='Ok']"));
-    public IClickable CloseButton => new Button("", new Locator(LocatorType.XPath, "//div[@aria-labelledby='ui-dialog-title-settingsDialog'][contains(@style, 'block')]//ul[@id='settings_tabs']//span[text()='close']"));
-    public IClickable NonDisplayedCloseButton => new Button("", new Locator(LocatorType.XPath, "//div[@aria-labelledby='ui-dialog-title-settingsDialog'][contains(@style, 'none')]//ul[@id='settings_tabs']//span[text()='close']"));
+    public TextField FullNameTextField => new TextField("", new Locator(LocatorType.Id, "FullNameInput"));
+    public TextField EmailTextField => new TextField("", new Locator(LocatorType.Id, "EmailInput"));
+    public Button OkButton => new Button("", new Locator(LocatorType.XPath, "//div[@class='ui-dialog-buttonset']//span[text()='Ok']"));
+    public Button CloseButton => new Button("", new Locator(LocatorType.XPath, "//div[@aria-labelledby='ui-dialog-title-settingsDialog'][contains(@style, 'block')]//ul[@id='settings_tabs']//span[text()='close']"));
+    public Button NonDisplayedCloseButton => new Button("", new Locator(LocatorType.XPath, "//div[@aria-labelledby='ui-dialog-title-settingsDialog'][contains(@style, 'none')]//ul[@id='settings_tabs']//span[text()='close']"));
 }
