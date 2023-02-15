e4641745aa10344e255dd46682aeacd807ef1127
Jorge Chavez <ljorchavez@gmail.com>
2023-02-15 13:18:03 -0600

    Added Update Item Test

--- 2.Views/WebAppPages/HomePage.cs
+++ 2.Views/WebAppPages/HomePage.cs
@@ -127,5 +127,26 @@ public class HomePage
             )
         );
 
+    public IClickable ItemButton(string itemName) =>
+        new Button(
+            "",
+            new Locator(
+                LocatorType.XPath,
+                "//div[@class='ItemContentDiv' and text()='" + itemName + "']"
+            )
+        );
+
+    public IClickable ProjectTd(string projectName) =>
+        new Button(
+            "",
+            new Locator(
+                LocatorType.XPath,
+                $"//div[@id='ProjectListPlaceHolder']//td[text()='{projectName}']"
+            )
+        );
+
+    public ITypeable ItemTextField =>
+        new TextField("", new Locator(LocatorType.Id, "ItemEditTextbox"));
+
     public HomePage() { }
 }
---
+++ 3.Tests/UI/StepDefinitions/Item/UpdateStepDefinition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using TechTalk.SpecFlow;
+using Todoly.Core.Helpers;
+using Todoly.Core.UIElements.Drivers;
+using Todoly.Core.UIElements.WebActions;
+using Todoly.Tests.UI.Steps.Commons;
+using Todoly.Views.WebAppPages;
+
+namespace SeleniumTest.Tests.Steps.Items;
+
+[Binding]
+[Scope(Feature = "Item Update")]
+public class UpdateStepDefinitions : CommonSteps
+{
+    private readonly HomePage _homePage;
+    private readonly ScenarioContext _scenarioContext;
+    private string _expectedItemName = "";
+
+    public UpdateStepDefinitions(ScenarioContext scenarioContext)
+        : base(scenarioContext)
+    {
+        _homePage = new HomePage();
+        _scenarioContext = scenarioContext;
+    }
+
+    [When(@"the user has selected a project")]
+    public void SelectProject()
+    {
+        _scenarioContext.TryGetValue(ConfigModel.CurrentProject, out string projectName);
+        _homePage.ProjectTd(projectName).Click();
+    }
+
+    [When(@"the user clicks on the item")]
+    public void ClickItem()
+    {
+        _scenarioContext.TryGetValue(ConfigModel.CurrentItem, out string itemName);
+        _homePage.ItemButton(itemName).Click();
+    }
+
+    [When(@"inputs a new item name and press enter")]
+    public void InputsNewItemName()
+    {
+        string itemName = IdHelper.GetNewId();
+        _expectedItemName = itemName;
+
+        _homePage.ItemTextField.Clear();
+        _homePage.ItemTextField.Type(itemName);
+        _homePage.ItemTextField.Type(Keys.Enter);
+    }
+
+    [Then(@"the item should be displayed with the new name")]
+    public void VerifyItemUpdate()
+    {
+        var xpath = "//div[@class='ItemContentDiv' and text()='" + _expectedItemName + "']";
+        var newItem = GenericWebDriver.Instance.FindElement(By.XPath(xpath));
+        Assert.True(newItem.Displayed);
+    }
+}
